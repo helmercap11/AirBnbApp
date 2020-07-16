03d9e21b32cda4e045258baf65bbb62a1321c3b6
helmercap11 <helmercap@gmail.com>
2020-07-16 15:31:35 +0100

    create pages and connect api

---
+++ src/pages/signIn/index.js
@@ -0,0 +1,53 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+import {StatusBar, AsyncStorage} from 'react-native';
+import {StackActions, NavigationActions} from 'react-navigation';
+
+import api from '../../services/api';
+
+import {
+  Container,
+  Logo,
+  Input,
+  ErrorMessage,
+  Button,
+  ButtonText,
+  SignUpLink,
+  SignUpLinkText,
+} from './styles';
+
+export default class SignIn extends Component {
+  render() {
+    return (
+      <Container>
+        <StatusBar hidden />
+        <Logo source={require('')} resizeMode="conta" />
+        <Input
+          placeholder="Endereço de e-mail"
+          value={this.state.email}
+          onChangeText={this.handleEmailChange}
+          autoCapitalize="none"
+          autoCorrect={false}
+        />
+        <Input
+          placeholder="senha"
+          value={this.state.senha}
+          onChangeText={this.handlePasswordChange}
+          autoCapitalize="none"
+          autoCorrect={false}
+          secureTextEntry
+        />
+        {this.state.error.length !== 0 && (
+          <ErrorMessage>{this.state.error}</ErrorMessage>
+        )}
+        <Button onPress={this.handleSignInPress}>
+          <ButtonText>Entrar</ButtonText>
+        </Button>
+        <SignUpLink onPress={this.handleCreateAccountPress}>
+          <SignUpLinkText>Criar conta grátis</SignUpLinkText>
+        </SignUpLink>
+      </Container>
+    );
+  }
+}
